b2c99f81d5d74be204c304cb6031fc7177c84837
Jason Lee <jasonylee.242@gmail.com>
2024-10-17 23:46:22 -0700

    temp removal, feature in development

--- dist/background.js
+++ dist/background.js
@@ -1,25 +1,22 @@
 "use strict";
-console.log("Feature in Development!")
-// Background worker to track tab changes
-//In Development
-
-
+// // Background worker to track tab changes
 // chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-//     const youtubeRegex = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/.+$/;
-//     if (changeInfo.status === "complete" &&
-//         tab.url &&
-//         youtubeRegex.test(tab.url)) {
-//         const queryParameters = tab.url.split("?")[1];
-//         const urlParameters = new URLSearchParams(queryParameters);
-//         const videoId = urlParameters.get("v");
-//         chrome.runtime.sendMessage(tabId, {
-//             type: "YT_TAB",
-//             videoUrl: `https://www.youtube.com/watch?v=${videoId}`,
-//         });
-//     }
-//     else if (changeInfo.status === "complete" && tab.url) {
-//         chrome.runtime.sendMessage(tabId, {
-//             type: "NO_YT_TAB",
-//         });
-//     }
+//   const youtubeRegex = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/.+$/;
+//   if (
+//     changeInfo.status === "complete" &&
+//     tab.url &&
+//     youtubeRegex.test(tab.url)
+//   ) {
+//     const queryParameters = tab.url.split("?")[1];
+//     const urlParameters = new URLSearchParams(queryParameters);
+//     const videoId = urlParameters.get("v");
+//     chrome.tabs.sendMessage(tabId, {
+//       type: "YT_TAB",
+//       videoUrl: `https://www.youtube.com/watch?v=${videoId}`,
+//     });
+//   } else if (changeInfo.status === "complete" && tab.url) {
+//     chrome.tabs.sendMessage(tabId, {
+//       type: "NO_YT_TAB",
+//     });
+//   }
 // });
